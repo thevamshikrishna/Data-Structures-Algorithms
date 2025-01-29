79c693eb8a08078659964097facafbe219e3923e
VamshiNallavalli <vamshikrishnaiam7@gmail.com>
2025-01-29 12:36:42 +0530

    Updated

--- Java Basic Programs/ascii_Number.java
+++
@@ -1,7 +0,0 @@
-public class ascii_Number {
-    public static void main(String args[]){
-        char x='a';
-        int ascci1=x;
-        System.out.println(ascci1);
-    }
-}
--- LinkedList/Main1.java
+++
@@ -1,6 +0,0 @@
-public class Main1 {
-    public static void main(String args[]){
-        LinkedList26 obj=new LinkedList26();
-        obj.method26();
-    }
-}
