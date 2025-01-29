23aae1c6689d4fc6ee525b2a5d110fa4fe399d89
Vamshi Nallavalli <101942717+thevamshikrishna@users.noreply.github.com>
2025-01-29 12:11:19 +0530

    Encapsulation.java

--- OOPS/Encapsulation/Encapsulation.java
+++ OOPS/Encapsulation/Encapsulation.java
@@ -3,3 +3,31 @@ public class Encapsulation{
 		support obj=new support("vamshi",69);
 	}
 }
+
+public class support{
+	private String name;
+	private int age;
+	
+	public void setName(String name){
+		this.name=name;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public void setAge(int age){
+		this.age=age;
+	}
+	
+	public int getAge(){
+		return age;
+	}
+	
+	public support(String name,int age){
+		this.name=name;
+		this.age=age;
+		
+		System.out.println("Hero Name is: "+getName()+" and Age is "+getAge());
+	}
+}
